2a31d5ce3cea9b3205b82730197214f8082538ca
Sanika Sree A K <145793335+sanika-ak@users.noreply.github.com>
2025-01-26 18:33:44 +0530

    Python Data Types.py
    This program shows how to use Python variables and data types. It involves assigning different types of values to variables and then displaying those values along with their corresponding data types.

---
+++ Python Data Types.py
@@ -0,0 +1,22 @@
+#VARIABLES OF DIFFERENT DATA TYPES
+a=532
+b=42.5
+c=True
+d=10+5j
+e="Hello"
+f=[1,2,3,4,5]
+g=(1,2,3,4,5)
+h={1,2,3,4,5}
+i={"Eng":45,"Mat":52,"Sci":53}
+
+#PRINTING THE VARIABLE TYPES WITH ITS VALUE
+#  Used Function "type()"
+print("a:",a ,type(a))
+print("b:",b ,type(b))
+print("c:",c ,type(c))
+print("d:",d ,type(d))
+print("e:",e ,type(e))
+print("f:",f ,type(f))
+print("g:",g ,type(g))
+print("h:",h ,type(h))
+print("i:",i ,type(i))
